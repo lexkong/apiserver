9bde6d58dcc8f7115ac58475b8b72fd8165e7c81
Lingfei Kong <786666882@qq.com>
2018-06-04 13:38:34 +0800

    add tls

--- main.go
+++ main.go
@@ -56,6 +56,16 @@ func main() {
 		log.Info("The router has been deployed successfully.")
 	}()
 
+	// Start to listening the incoming requests.
+	cert := viper.GetString("tls.cert")
+	key := viper.GetString("tls.key")
+	if cert != "" && key != "" {
+		go func() {
+			log.Infof("Start to listening the incoming requests on https address: %s", viper.GetString("tls.addr"))
+			log.Info(http.ListenAndServeTLS(viper.GetString("tls.addr"), cert, key, g).Error())
+		}()
+	}
+
 	log.Infof("Start to listening the incoming requests on http address: %s", viper.GetString("addr"))
 	log.Info(http.ListenAndServe(viper.GetString("addr"), g).Error())
 }
