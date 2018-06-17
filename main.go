6a94829164cf0bf5c306e7c5cd37d2db6902fc25
Lingfei Kong <786666882@qq.com>
2018-06-18 02:29:54 +0800

    update swagger

--- main.go
+++ main.go
@@ -26,6 +26,16 @@ var (
 	version = pflag.BoolP("version", "v", false, "show version info.")
 )
 
+// @title Apiserver Example API
+// @version 1.0
+// @description apiserver demo
+
+// @contact.name lkong
+// @contact.url http://www.swagger.io/support
+// @contact.email 466701708@qq.com
+
+// @host localhost:8080
+// @BasePath /v1
 func main() {
 	pflag.Parse()
 	if *version {
